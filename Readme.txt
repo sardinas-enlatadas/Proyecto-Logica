1a04e0e6ec0e6bee7a258f27e935cc5d17804368
Ricardo <ricardo_glz@ciencias.unam.mx>
2019-03-16 19:49:26 -0600

    Primer intento

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,37 @@
 
+pasarAFNC:: Prop -> Prop
+pasarAFNC a = fnc(fnn a)
+
+fnc :: Prop -> Prop
+fnc TTrue = TTrue
+fnc FFalse = FFalse
+fnc (Var p) = Var p
+
+fnc z@(Neg (Var a)) = z
+fnc z@(Conj (Var p) (Var q))= z
+fnc z@(Conj (Var p) (Neg q)) = z
+fnc (Conj (Neg q) (Var p)) = fnc (Conj (Var p) (Neg q))
+
+fnc (Conj (Disy a b) (Var q) ) = Conj (fnc (Disy a b)) (Var q)
+fnc (Conj (Var p) (Disy a b)) = fnc (Conj (Disy a b) (Var p))
+fnc (Conj x@(Disy a b) y@(Disy c d)) = Conj (fnc x) (fnc y)
+
+fnc (Conj (Conj a b) (Var p)) = Conj (pasarAFNC (Disy (Neg a) (Neg b) ) ) (Var p)
+fnc (Conj (Var p) (Conj a b)) = fnc (Conj (Conj a b) (Var p))
+fnc (Conj x@(Conj a b) y@(Conj c d)) = Conj (pasarAFNC (Neg x )) (pasarAFNC (Neg y))
+
+fnc (Disy (Var p) (Var q)) = Conj (Neg (Var p) ) (Neg (Var q))
+fnc (Disy (Neg p) (Var q)) = Conj p (Var q)
+fnc (Disy (Var p) (Neg q)) = fnc (Disy (Neg q) (Var p))
+
+fnc (Disy (Conj a b) (Var p) )= Conj (fnc (Disy a (Var p))) (fnc (Disy b (Var p)))
+fnc (Disy (Var p) (Conj a b)) = fnc (Disy (Conj a b) (Var p))
+fnc (Disy (Conj a b) (Conj c d)) = Conj (pasarAFNC (Disy (Neg a ) (Neg b)) )  (pasarAFNC (Disy (Neg c) (Neg d) ))
+
+fnc x@(Disy (Disy a b) (Var p)) =  pasarAFNC (Neg x)
+fnc (Disy (Var p) (Disy a b)) = fnc (Disy (Disy a b) (Var p))
+fnc x@(Disy (Disy a b) (Disy c d)) = pasarAFNC (Neg x)
+--fnc (Disy (Disy a b) (Var p) ) =
+
+  -- (pvq) v (t)
+  -- (-p ^ -q)  ^ -t
--- Tableaux.hs
+++ Tableaux.hs
@@ -1,6 +1,7 @@
 module  Tableaux where
 
   -- Tipo de dato para representar expresiones de la lógica proposicional
+
 data Prop = TTrue
           | FFalse
           | Var String
@@ -22,21 +23,6 @@ instance Show Prop where
     show (Impl p q) = "(" ++ show p ++ " → " ++ show q ++ ")" -- (P → Q)
     show (Syss p q) = "(" ++ show p ++ " ↔ " ++ show q ++ ")" -- (P ↔ Q)
 
-pasarAFNC:: Prop -> Prop
-pasarAFNC a = fnc(fnn a)
-
-fnc :: Prop -> Prop
-fnc TTrue = TTrue
-fnc FFalse = FFalse
-fnc (Var p) = Var p
-fnc (Neg (Var a)) = Neg(Var a)
-fnc z@(Conj (Var p) (Var q))= z
-fnc z@(Conj (Var p) (Not q)) = z
-fnc z@(Conj (Not q) (Var p)) = fnc (Conj (Var p) (Not q)) 
-
-fnc (Conj (Disy a b) (Var q) ) = Conj (fnc (Disy a b)) (Var q)
-fnc (Conj (Var p) (Disy a b)) = fnc (Conj (Disy a b) (Var p))
-fnc (Conj x@(Disy a b) y@(Disy c d)) = Conj (fnc x) (fnc y)
 fnn :: Prop -> Prop
 fnn TTrue = TTrue
 fnn FFalse = FFalse
@@ -52,3 +38,53 @@ fnn (Conj p q) = Conj (fnn p) (fnn q)
 fnn (Disy p q) = Disy (fnn p) (fnn q)
 fnn (Impl p q) = Disy (fnn (Neg p)) (fnn q)
 fnn (Syss p q) = Conj (fnn (Impl p q)) (fnn (Impl q p))
+--Busca si en la lista que le pasan existe una formula estilo p ^ -p
+--Si la encuentra devuelve True, si no, False
+hayContradiccion :: [Prop]-> [Prop] -> Bool
+hayContradiccion _ [] = False
+hayContradiccion [] _ = False
+hayContradiccion (a:as) l = if elem (Neg a) l then True else (hayContradiccion as l)
+--Metodo principal. Es el que llamas a la hora de buscar si la lista de proposiciones es satisfacible ,
+--Pasa la lista de props a fnn para que puedan ejecutarse las reglas alfa y beta.
+tableaux :: [Prop] -> Bool
+tableaux [] = True
+tableaux (x:xs) = tableaux1(map (fnn) (x:xs))
+--Recibe una lista de proposiciones y devuelve si existe un conjunto de estados tal que sea satisfacible
+--Primero busca una contradiccion entre las variables libres dentro del conjunto. Si la encuentra, devuelve False
+--Si no, manda a llamar a tableauxAux
+tableaux1 :: [Prop] -> Bool
+tableaux1 [] = True
+tableaux1 (x:xs)
+  | hayContradiccion (x:xs) (x:xs) = False
+  | otherwise = tableauxAux (ordena (x:xs)) (ordena (x:xs))
+--Recibe dos listas. La primera es sobre la que va a trabajar, la segunda es la lista completa de proposiciones originales
+--Si se da el caso de que se reciba un Var p, manda a llamar a cierraRamas con la lista de proposiciones originales
+--Si no es una variable, se manda a llamar recursivamente hasta que encuentre una Var
+--Hay dos casos recursivos. Que haya una disyuncion o una conjuncion
+-- Si hay una conjuncion busca que ambas ramas no se cierren
+-- Si hay una disyuncion busca que por lo menos una se mantenga abierta
+tableauxAux :: [Prop]-> [Prop] -> Bool
+tableauxAux _ [] =True
+tableauxAux [] _ = True
+tableauxAux ((Var p):xs) l = not (cierraRamas (Var p) l) && tableauxAux xs l
+tableauxAux ((Neg p):xs) l = cierraRamas (Neg p) l && tableauxAux xs l
+tableauxAux ((Conj x@(Var a) y@(Var b)):xs) l = cierraRamas x l && cierraRamas y l && tableauxAux xs l
+tableauxAux ((Conj x@(Var a) b):xs) l = (cierraRamas x l) && (tableauxAux [b] l) && (tableauxAux xs l)
+tableauxAux ((Conj a x@(Var b)):xs) l = tableauxAux [a] l && cierraRamas x l && tableauxAux xs l
+tableauxAux ((Conj a b):xs) l = tableauxAux [a] l && tableauxAux [b] l && tableauxAux xs l
+tableauxAux ((Disy x@(Var a) y@(Var b)):xs) l = cierraRamas x l || cierraRamas y l && tableauxAux xs l
+tableauxAux ((Disy a x@(Var b) ):xs) l = tableauxAux [a] l || cierraRamas x l && tableauxAux xs  l
+tableauxAux ((Disy x@(Var a) b):xs) l = cierraRamas x l || tableauxAux [b] l && tableauxAux xs l
+tableauxAux ((Disy a b):xs) l= tableauxAux [a] l || tableauxAux [b] l && tableauxAux xs l
+
+--Devuelve True si la rama se cerro
+
+cierraRamas :: Prop -> [Prop] -> Bool
+cierraRamas x l = (elem (Neg x) l)
+--Pone primero las variables, las conjunciones y al final las disyunciones
+ordena:: [Prop] -> [Prop]
+ordena [] = []
+ordena ((Var p):xs )= (Var p):(ordena xs)
+ordena ((Neg a):xs )= (Neg a):(ordena xs)
+ordena ((Conj a b):xs )= (Conj a b):(ordena xs)
+ordena ((Disy a b):xs) = (ordena xs)++[(Disy a b)]
