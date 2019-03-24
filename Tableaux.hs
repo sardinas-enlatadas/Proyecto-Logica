b5bb7a6d466922f5e1ddd419fcaea9060e22f13c
Ricardo <ricardo_glz@ciencias.unam.mx>
2019-03-24 00:46:42 -0600

    Arreglado el sumar

--- Tableaux.hs
+++ Tableaux.hs
@@ -1,30 +1,25 @@
 module  Tableaux where
 
-  -- Tipo de dato para representar expresiones de la lógica proposicional
-
-data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show, Ord, Eq)
-leaf x = Branch x Empty Empty
-
 data Prop = TTrue
-          | FFalse
-          | Var String
-          | Neg Prop
-          | Conj Prop Prop -- (P ∧ Q)
-          | Disy Prop Prop -- (P ∨ Q)
-          | Impl Prop Prop -- (P → Q)
-          | Syss Prop Prop -- (P ↔ Q)
-  				deriving (Eq,Ord)
+            | FFalse
+            | Var String
+            | Neg Prop
+            | Conj Prop Prop -- (P ∧ Q)
+            | Disy Prop Prop -- (P ∨ Q)
+            | Impl Prop Prop -- (P → Q)
+            | Syss Prop Prop -- (P ↔ Q)
+    				deriving (Eq,Ord)
 
-  -- show de Prop
+-- show de Prop
 instance Show Prop where
-    show TTrue = "True" -- T
-    show FFalse = "False" -- F
-    show (Var x) = x -- P
-    show (Neg p) = "¬ " ++ show p -- ¬ P
-    show (Conj p q) = "(" ++ show p ++ " ∧ " ++ show q ++ ")" -- (P ∧ Q)
-    show (Disy p q) = "(" ++ show p ++ " ∨ " ++ show q ++ ")" -- (P ∨ Q)
-    show (Impl p q) = "(" ++ show p ++ " → " ++ show q ++ ")" -- (P → Q)
-    show (Syss p q) = "(" ++ show p ++ " ↔ " ++ show q ++ ")" -- (P ↔ Q)
+      show TTrue = "True" -- T
+      show FFalse = "False" -- F
+      show (Var x) = x -- P
+      show (Neg p) = "¬ " ++ show p -- ¬ P
+      show (Conj p q) = "(" ++ show p ++ " ∧ " ++ show q ++ ")" -- (P ∧ Q)
+      show (Disy p q) = "(" ++ show p ++ " ∨ " ++ show q ++ ")" -- (P ∨ Q)
+      show (Impl p q) = "(" ++ show p ++ " → " ++ show q ++ ")" -- (P → Q)
+      show (Syss p q) = "(" ++ show p ++ " ↔ " ++ show q ++ ")" -- (P ↔ Q)
 
 fnn :: Prop -> Prop
 fnn TTrue = TTrue
@@ -41,6 +36,19 @@ fnn (Conj p q) = Conj (fnn p) (fnn q)
 fnn (Disy p q) = Disy (fnn p) (fnn q)
 fnn (Impl p q) = Disy (fnn (Neg p)) (fnn q)
 fnn (Syss p q) = Conj (fnn (Impl p q)) (fnn (Impl q p))
+-- Tipo de dato para representar expresiones de la lógica proposicional
+
+data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show, Ord, Eq)
+leaf x = Branch x Empty Empty
+--El dato Crumb nos guarda el nodo del que nos movimos (papa) y el arbol que no pudimos visitar
+-- Por ejemplo, el LeftCrumb es el nodo padre y el arbol derecho que no visitamos
+data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving (Show)
+--Tanto BreadCrumbs como Zipper son azucar sintactico
+--BreadCrumbs es para guardar un camino hasta la raiz
+type BreadCrumbs a = [Crumb a]
+-- Zipper es una tupla, la primera entrada es el arbol actual y la segunda
+-- es todo lo que hay arriba del nodo actual
+type Zipper a= (Tree a, BreadCrumbs a)
 --Pasa una proposicion a su forma de arbol. Supone que ya esta en fnn
 --Las conjunciones las mete en el arbol derecho, las disyunciones las divide en dos ramas.
 --Guarda la proposicion original como el nodo padre.
@@ -56,7 +64,7 @@ alfaRegla :: Prop -> Tree Prop
 alfaRegla (Conj a (Var p)) = Branch (Conj a (Var p)) Empty (Branch (Var p) Empty (arboliza a))
 alfaRegla (Conj (Var p) a) = Branch (Conj (Var p) a) Empty (Branch (Var p) Empty (arboliza a))
 alfaRegla (Conj a b) = Branch (Conj a b) Empty (sumados)
-  where sumados = suma (sacaDer(arboliza a)) (arboliza b)
+  where sumados = suma (arboliza a) (arboliza b)
 
 --Es una extension a la alfaRegla. La idea es que al ir agregando cada prop revise si su negacion ya pertenece al arbol
 --Aun es necesario meter la proposicion a revisar a mano en la consola para probarla
@@ -65,63 +73,26 @@ sigueAbierta _ Empty = True
 sigueAbierta a (Branch b Empty Empty) = if (Neg a) == b then False else True
 sigueAbierta a (Branch x t1 t2) =
     if esta (Neg(a))  (Branch x t1 t2) then False else True
-
---Es la funcion que sirve para buscar si una proposicion ya esta dentro del arbol
+--Es la funcion que sirve para busca r si una proposicion ya esta dentro del arbol
 esta :: Prop -> Tree Prop -> Bool
 esta x Empty = False
 esta x (Branch a Empty Empty) = x == a
 esta x (Branch a t1 t2) = if x == a || esta x t1  || esta x t2 then True else False
-
 --Nos regresa el elemento mas a la derecha del arbol
-sacaDer :: Tree Prop ->Tree Prop
-sacaDer y@(Branch x Empty Empty) = y
+sacaDer :: Tree Prop ->Prop
+sacaDer Empty = TTrue
+sacaDer y@(Branch x Empty Empty) = x
 sacaDer (Branch x _ t2) = sacaDer t2
-
---Le agrega un arbol completo en la rama derecha a un arbol. No fue necesario poner mas casos pues la unica funcion que manda a
+--Le agrega un arbol completo en la rama derecha a un arbol.
 --llamar a esta funcion es la que mete conjunciones
 suma :: Tree Prop -> Tree Prop ->Tree Prop
 suma (Branch x Empty Empty) y = Branch x Empty y
+suma (Branch a t1 t2) y = Branch a t1 (suma t2 y)
 
 --La funcion que mete las disyunciones
 betaRegla ::Prop -> Tree Prop
 betaRegla (Disy a b) = Branch (Disy a b) (arboliza a) (arboliza b)
 
-----------------------------------------------------------------
---Busca la proposicion en el arbol, si se encuentra devuelve el subarbol
---Que contiene a ese elemento, si no, regresa 'Empty'
-busca :: Prop -> Tree Prop -> Tree Prop
-busca x Empty = error"El elemento no esta en el arbol"
-busca x (Branch a t1 t2)
-        |x == a =(Branch a t1 t2)
-        |esta x t1 = busca x t1
-        |esta x t2 = busca x t2
-        |otherwise = Empty
-
-colectaElementos :: Tree Prop -> [Prop]
-colectaElementos Empty = []
-colectaElementos (Branch TTrue t1 t2) = [TTrue] ++ colectaElementos t1 ++colectaElementos t2
-colectaElementos (Branch FFalse t1 t2) = [FFalse] ++ colectaElementos t1 ++colectaElementos t2
-colectaElementos (Branch (Var x) t1 t2) = [(Var x)] ++ colectaElementos t1 ++ colectaElementos t2
-colectaElementos (Branch (Neg(Var(x))) t1 t2)= [(Neg(Var(x)))]++ colectaElementos t1 ++ colectaElementos t2
-colectaElementos (Branch e t1 t2)= colectaElementos t1 ++ colectaElementos t2
-
-elimina:: (Eq a) => a->[a] -> [a]
-elimina e [] = []
-elimina e (x:xs)
-          |x==e = xs
-          |otherwise=[x] ++ elimina e (xs)
-
-revisarNegaciones:: [Prop]-> Tree Prop->[Bool]
-revisarNegaciones [] t = []
-revisarNegaciones (x:xs) (Branch e t1 t2) = [esta (Neg(x)) (busca x ((Branch e t1 t2)))]++revisarNegaciones (elimina (Neg(x)) (xs)) (Branch e t1 t2)
-
-tablo:: Prop -> [Bool]
-tablo p= revisarNegaciones (colectaElementos (arboliza (fnn(p)))) (arboliza(fnn(p)))
---colectaElementos
-{-
-inOrder:: Tree Prop -> [Prop]
-inOrder Empty = []
-inOrder (Branch (Var x) r l) = (inOrder r)++[(Var x)]++(inOrder l)
-inOrder (Branch (Neg(Var(x))) r l)=(inOrder r)++[Neg(Var x)]++(inOrder l)
-inOrder (Branch e r l)=(inOrder r)++(inOrder l)
--}
+moverseArriba :: Zipper a -> Zipper a
+moverseArriba (t, LeftCrumb x r:bs) = (Branch x t r, bs)
+moverseArriba (t, RightCrumb x l:bs) = (Branch x l t, bs)
